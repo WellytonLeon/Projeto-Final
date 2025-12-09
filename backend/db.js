b1f8df81a8187a342494fc1bb0d700fffa9afb48
carlos <carlos.santos16@aluno.senai.br>
2025-12-09 10:46:03 -0300

    alteração no backend - Modificação no Banco e integração para multiuso do Banco

--- backend/db.js
+++ backend/db.js
@@ -1,17 +1,20 @@
 const mysql = require("mysql2");
+require('dotenv').config();
 
 const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "12345678", 
-    database: "projeto_final"
+    host: process.env.host,
+    user: process.env.user,
+    password: process.env.password, 
+    database: process.env.database
 });
 
 connection.connect((err) => {
     if (err) {
+            console.log(process.env.host);
         console.error("Erro ao conectar ao MySQL:", err);
         return;
     }
+
     console.log("Conectado ao MySQL!");
 });
 
